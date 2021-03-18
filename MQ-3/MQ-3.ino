22b9adecef72edf8d424c94b5ac6a640de16e6c7
michael61106 <80897231+michael61106@users.noreply.github.com>
2021-03-18 21:38:02 +0800

    Update MQ-3.ino

--- MQ-3/MQ-3.ino
+++ MQ-3/MQ-3.ino
@@ -1,31 +1,25 @@
-int sensorPin = A0;    // select the input pin for the potentiometer
-int ledPin = 13;      // select the pin for the LED
-int sensorValue = 0;  // variable to store the value coming from the sensor
+//
+// MQ3: Alcohol Sensor
+//
+//int TIME_UNTIL_WARMUP = 900;
+//unsigned long time;
 
 void setup() {
-  // declare the ledPin as an OUTPUT:
-  pinMode(ledPin, OUTPUT);
   Serial.begin(115200);
+  pinMode(A0, INPUT);
+
+  // warm-up MQ-3 for 15 minutes
+  //time = millis()/1000;
+  //while(time<=TIME_UNTIL_WARMUP);
+  
+  // warm-up MQ-3 till reading value <600
+  
 }
 
-void loop() 
-{
-  sensorValue = analogRead(sensorPin);
-  Serial.println(sensorValue);
+void loop() {
+  int alco;
+  alco = analogRead(A0);
+  Serial.println(alco);
 }
-void Thermistor(int16_t ADCvalue)
-{
-  double T, Temp;
-  double T0 = 301.15;
-  double lnR;
-  int16_t R; 
-  int16_t R0 = 8805;
-  int16_t B  = 3950;
-  int16_t Pullup = 9930; // 10K ohm
-  R = (Pullup * ADCvalue) / (4096 -ADCvalue);
-  T = 1 / (1/T0 + (log(R)-log(R0)) / B );
-  Temp = T -273.15;
-  printf("ADC:%4d, R=%d, Temp.=%f\n", ADCvalue, R, Temp);
-}
-
+© 2021 GitHub, Inc.
 
